b59a77eb802e81e3450406fc5f6205f57175d1aa
Son Luong Ngoc <sluongng@gmail.com>
2019-07-17 09:38:26 +0700

    Moved dir to variables for easier management

--- init.sh
+++ init.sh
@@ -18,64 +18,67 @@
 # - Provisioned all configuration files
 
 DIR_DOTFILE=~/.dotfiles
+DIR_HOME=${HOME}
+DIR_CONFIG=${DIR_HOME}/.config
+
 
 # Clone repo
-if [ ! -d "~/.dotfiles" ]; then
+if [ ! -d "${DIR_HOME}/.dotfiles" ]; then
   git clone git@github.com:sluongng/dotfiles.git ${DIR_DOTFILE}
 else 
   echo 'Directory .dotfiles already exist, pulling instead of cloning'
-  git -C ~/.dotfiles pull
+  git -C ${DIR_HOME}/.dotfiles pull
 fi
 
 # Backup existing files
-if [ -f "~/.zshrc" ]; then
-  cp ~/.zshrc ~/.zshrc.bak
+if [ -f "${DIR_HOME}/.zshrc" ]; then
+  cp ${DIR_HOME}/.zshrc ${DIR_HOME}/.zshrc.bak
   echo 'Done back up zsh config'
 else
   echo 'No .zshrc found'
 fi
 
-if [ -f "~/.vimrc" ]; then
-  cp ~/.vimrc ~/.vimrc.bak
+if [ -f "${DIR_HOME}/.vimrc" ]; then
+  cp ${DIR_HOME}/.vimrc ${DIR_HOME}/.vimrc.bak
   echo 'Done back up vim config'
 else
   echo 'No .vimrc found'
 fi
 
-if [ -f "~/config/nvim/init.vim" ]; then
-  cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak
+if [ -f "${DIR_CONFIG}/nvim/init.vim" ]; then
+  cp ${DIR_CONFIG}/nvim/init.vim ${DIR_CONFIG}/nvim/init.vim.bak
   echo 'Done back up neovim config'
 else
   echo 'No init.vim(neovim config) found'
 fi
 
-if [ -f "~/config/nvim/coc-settings.json" ]; then
-  cp ~/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json.bak
+if [ -f "${DIR_CONFIG}/nvim/coc-settings.json" ]; then
+  cp ${DIR_CONFIG}/nvim/coc-settings.json ${DIR_CONFIG}/nvim/coc-settings.json.bak
   echo 'Done back up coc.nvim config'
 else
   echo 'No coc-config.json(coc.nvim config) found'
 fi
 
-if [ -f "~/.tmux.conf" ]; then
-  cp ~/.tmux.conf ~/.tmux.conf.bak
+if [ -f "${DIR_HOME}/.tmux.conf" ]; then
+  cp ${DIR_HOME}/.tmux.conf ${DIR_HOME}/.tmux.conf.bak
   echo 'Done back up tmux config'
 else
   echo 'No .tmux.conf(tmux config) found'
 fi
 
 # Provisioning directories
-mkdir -p ~/.config/nvim/
+mkdir -p ${DIR_CONFIG}/nvim/
 
 # Link all the dotfiles
-ln -sfn ${DIR_DOTFILE}/.zshrc ~/.zshrc
+ln -sfn ${DIR_DOTFILE}/.zshrc ${DIR_HOME}/.zshrc
 echo 'Linked .zshrc from dotfiles'
 
-ln -sfn ${DIR_DOTFILE}/.vimrc ~/.vimrc
-ln -sfn ${DIR_DOTFILE}/.vimrc ~/.config/nvim/init.vim
+ln -sfn ${DIR_DOTFILE}/.vimrc ${DIR_HOME}/.vimrc
+ln -sfn ${DIR_DOTFILE}/.vimrc ${DIR_CONFIG}/nvim/init.vim
 echo 'Linked .vimrc from dotfiles'
 
-ln -sfn ${DIR_DOTFILE}/config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sfn ${DIR_DOTFILE}/config/nvim/coc-settings.json ${DIR_CONFIG}/nvim/coc-settings.json
 echo 'Linked coc-settings.json from dotfiles'
 
-ln -sfn ${DIR_DOTFILE}/.tmux.conf ~/.tmux.conf
+ln -sfn ${DIR_DOTFILE}/.tmux.conf ${DIR_HOME}/.tmux.conf
 echo 'Linked .tmux.conf from dotfiles'
