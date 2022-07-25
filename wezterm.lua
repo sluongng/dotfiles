afd5e74cc98c1ed33f786c9c173d875e0f68448c
Son Luong Ngoc <sluongng@gmail.com>
2022-07-25 11:56:38 +0200

    wezterm: added quick link open
    This is a feature that I used a lot from Kitty, let's add it to wezterm.

--- wezterm.lua
+++ wezterm.lua
@@ -39,6 +39,20 @@ return {
         { key = 'l', mods = 'LEADER', action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain'} },
         { key = 'j', mods = 'LEADER', action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain'} },
 
+        -- Quick open http links using Ctrl + Shift + E
+        {
+            key="e", mods="CTRL|SHIFT",
+            action=wezterm.action{QuickSelectArgs={
+                patterns={
+                   "https?://\\S+"
+                },
+                action = wezterm.action_callback(function(window, pane)
+                   local url = window:get_selection_text_for_pane(pane)
+                   wezterm.log_info("opening: " .. url)
+                   wezterm.open_with(url)
+                end)
+            }}
+        },
 
         -- **Editor/Shell navigations**
         { key = 'a', mods = 'SUPER', action = wezterm.action{SendString = '\x1ba'} },
